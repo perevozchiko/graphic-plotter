b756828a9648ea7809bfd1854323c671a1198f2d
Andrey Perevozchikov <lo-st@mail.ru>
2019-07-11 09:59:11 +0400

    мелкие правки

--- validator.h
+++ validator.h
@@ -8,16 +8,10 @@ class Validator : public QValidator
     Q_OBJECT
 public:
     Validator(QObject *parent = nullptr);
-
-signals:
-
-public slots:
+    State validate(QString& expression, int& pos) const override;
 
 private:
     QString expression;
-
-public:
-    State validate(QString& expression, int& pos) const override;
 };
 
 #endif // VALIDATOR_H
